e2c32be54a59461b648cd7165343b9f67bee2596
zakwarren <41547855+zakwarren@users.noreply.github.com>
2021-08-26 17:02:25 +0100

    ingress nginx setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,5 +3,9 @@ import express, { json } from "express";
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hello world!");
+});
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port ${port}`));
