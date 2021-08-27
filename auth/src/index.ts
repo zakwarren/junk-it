5bf2c8130c4c59d3582070ff90533ebfd29f8ced
zakwarren <41547855+zakwarren@users.noreply.github.com>
2021-08-27 18:03:02 +0100

    Set up error handling middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { singoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middleware/error-handler";
 
 const app = express();
 app.use(json());
@@ -13,6 +14,8 @@ app.use(rootRoute, currentUserRouter);
 app.use(rootRoute, signinRouter);
 app.use(rootRoute, singoutRouter);
 app.use(rootRoute, signupRouter);
+
+app.use(errorHandler);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port ${port}`));
