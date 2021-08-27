41c2e74d09c9496378e70e9f37b321e1bb8f310d
zakwarren <41547855+zakwarren@users.noreply.github.com>
2021-08-27 17:03:02 +0100

    Validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,29 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = Router();
 
-router.post("/signup", (req, res) => {
-  res.send("Hello world!");
-});
+router.post(
+  "/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 8, max: 20 })
+      .withMessage("Password must be between 8 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    console.log("Creating a user...");
+
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
