076ceca3a1915bfd5b46d899f8fab0eaf2212870
zakwarren <41547855+zakwarren@users.noreply.github.com>
2021-09-01 11:45:34 +0100

    Connecting to mongo db

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express, { json } from "express";
 import "express-async-errors";
+import mongoose from "mongoose";
 
 import {
   currentUserRouter,
@@ -25,5 +26,16 @@ app.all("*", () => {
 
 app.use(errorHandler);
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-service:27017/auth");
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error(err);
+  }
+
+  const port = process.env.PORT || 3001;
+  app.listen(port, () => console.log(`Listening on port ${port}`));
+};
+
+start();
