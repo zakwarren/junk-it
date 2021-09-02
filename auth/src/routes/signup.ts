8684758c007403488e8184ada3e364273a419393
zakwarren <41547855+zakwarren@users.noreply.github.com>
2021-09-02 11:07:49 +0100

    Create user and detect if user exists

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,31 +1,41 @@
 import { Router, Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 
-import { RequestValidationError, DatabaseConnectionError } from "../errors";
+import { RequestValidationError } from "../errors";
+import { User } from "../models";
 
 const router = Router();
 
 router.post(
   "/signup",
   [
-    body("email").isEmail().withMessage("Email must be valid"),
+    body("email")
+      .isEmail()
+      .withMessage("Email must be valid")
+      .bail()
+      .custom(async (value) => {
+        const existingUser = await User.findOne({ email: value });
+        if (existingUser) {
+          throw new Error("Email in use");
+        }
+        return true;
+      }),
     body("password")
       .trim()
       .isLength({ min: 8, max: 20 })
       .withMessage("Password must be between 8 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
 
     const { email, password } = req.body;
+    const user = new User({ email, password });
+    await user.save();
 
-    console.log("Creating a user...");
-    throw new DatabaseConnectionError();
-
-    res.send({});
+    res.status(201).send(user);
   }
 );
 
