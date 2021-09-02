4bd6c789801169c98f7de1e36a6b2b0757163c49
zakwarren <41547855+zakwarren@users.noreply.github.com>
2021-09-02 11:40:30 +0100

    Import bcrypt

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -1,20 +1,13 @@
-import { scrypt, randomBytes } from "crypto";
-import { promisify } from "util";
+import bcrypt from "bcrypt";
 
-const scryptAsync = promisify(scrypt);
+const saltRounds = 12;
 
 export class Password {
   static async toHash(password: string) {
-    const salt = randomBytes(8).toString("hex");
-    const buff = (await scryptAsync(password, salt, 64)) as Buffer;
-
-    return `${buff.toString("hex")}.${salt}`;
+    return bcrypt.hash(password, saltRounds);
   }
 
   static async compare(storedPassword: string, suppliedPassword: string) {
-    const [hashedPassword, salt] = storedPassword.split(".");
-    const buff = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
-
-    return buff.toString("hex") === hashedPassword;
+    return bcrypt.compare(suppliedPassword, storedPassword);
   }
 }
