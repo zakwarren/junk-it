3d6f3606db512119873b1df9633ffff863190acf
zakwarren <41547855+zakwarren@users.noreply.github.com>
2021-09-03 14:35:53 +0100

    Require auth middleware

--- auth/src/errors/index.ts
+++ auth/src/errors/index.ts
@@ -2,5 +2,6 @@ export { CustomError } from "./custom-error";
 
 export { BadRequestError } from "./bad-request-error";
 export { DatabaseConnectionError } from "./database-connection-error";
+export { NotAuthorizedError } from "./not-authorized-error";
 export { NotFoundError } from "./not-found-error";
 export { RequestValidationError } from "./request-validation-error";
