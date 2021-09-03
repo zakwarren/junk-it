349b9851206c2f6c257f0082eeec909d598bd47e
zakwarren <41547855+zakwarren@users.noreply.github.com>
2021-09-03 15:14:18 +0100

    Refactor app

---
+++ auth/src/app.ts
@@ -0,0 +1,37 @@
+import express, { json } from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
+import {
+  currentUserRouter,
+  signinRouter,
+  singoutRouter,
+  signupRouter,
+} from "./routes";
+import { NotFoundError } from "./errors";
+import { errorHandler } from "./middleware";
+
+const app = express();
+// trust proxy as behind nginx, so should trust its traffic
+app.set("trust-proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV === "production",
+  })
+);
+
+const rootRoute = "/api/users";
+app.use(rootRoute, currentUserRouter);
+app.use(rootRoute, signinRouter);
+app.use(rootRoute, singoutRouter);
+app.use(rootRoute, signupRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
