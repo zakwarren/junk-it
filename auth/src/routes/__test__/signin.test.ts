98303cbcb985427c7179d48e5eee3830375d3fa9
zakwarren <41547855+zakwarren@users.noreply.github.com>
2021-09-03 16:07:11 +0100

    Test signin route handler

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,54 @@
+import request from "supertest";
+import { app } from "../../app";
+
+describe("signin route", () => {
+  it("should return a 400 with an invalid email", async () => {
+    return request(app)
+      .post("/api/users/signin")
+      .send({ email: "test", password: "password" })
+      .expect(400);
+  });
+
+  it("should return a 400 with no password", async () => {
+    return request(app)
+      .post("/api/users/signin")
+      .send({ email: "test@test.com" })
+      .expect(400);
+  });
+
+  it("should fail when an email that does not exist is supplied", async () => {
+    return request(app)
+      .post("/api/users/signin")
+      .send({ email: "test@test.com", password: "password" })
+      .expect(400);
+  });
+
+  it("should fail when an incorrect password is supplied", async () => {
+    await request(app)
+      .post("/api/users/signup")
+      .send({ email: "test@test.com", password: "password" })
+      .expect(201);
+
+    return request(app)
+      .post("/api/users/signin")
+      .send({ email: "test@test.com", password: "testing123" })
+      .expect(400);
+  });
+
+  it("should respond with a cookie when given valid credentials", async () => {
+    await request(app)
+      .post("/api/users/signup")
+      .send({
+        email: "test@test.com",
+        password: "password",
+      })
+      .expect(201);
+
+    const response = await request(app)
+      .post("/api/users/signin")
+      .send({ email: "test@test.com", password: "password" })
+      .expect(200);
+
+    expect(response.get("Set-Cookie")).toBeDefined();
+  });
+});
