abb428ea10d3bac6059906a3564b58e9d523632c
zakwarren <41547855+zakwarren@users.noreply.github.com>
2021-09-09 17:08:17 +0100

    useRequest hook

---
+++ client/hooks/useRequest.tsx
@@ -0,0 +1,40 @@
+import { useState, ReactElement } from "react";
+import axios from "axios";
+
+interface ValidationError {
+  message: string;
+  field?: string;
+}
+
+interface Request {
+  url: string;
+  method: "get" | "post" | "put" | "patch" | "delete";
+  body?: object;
+}
+
+export const useRequest = ({ url, method, body }: Request) => {
+  const [errors, setErrors] = useState<ReactElement>(null);
+
+  const doRequest = async () => {
+    try {
+      setErrors(null);
+
+      const response = await axios[method](url, body);
+      return response.data;
+    } catch (err) {
+      const errors: ValidationError[] = err?.response?.data?.errors || [];
+      setErrors(
+        <div className="alert alert-danger">
+          <h4>Something went wrong...</h4>
+          <ul className="my-0">
+            {errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  };
+
+  return { doRequest, errors };
+};
--- client/pages/auth/signup.tsx
+++ client/pages/auth/signup.tsx
@@ -1,23 +1,21 @@
 import { useState, FormEvent } from "react";
 import axios from "axios";
 
+import { useRequest } from "../../hooks";
+
 const signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    body: { email, password },
+  });
 
   const onSumbit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    try {
-      const response = await axios.post("/api/users/signup", {
-        email,
-        password,
-      });
-      console.log(response.data);
-    } catch (err) {
-      setErrors(err?.response?.data?.errors);
-    }
+    doRequest();
   };
 
   return (
@@ -39,16 +37,7 @@ const signup = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
-      {errors.length > 0 && (
-        <div className="alert alert-danger">
-          <h4>Something went wrong...</h4>
-          <ul className="my-0">
-            {errors.map((err) => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+      {errors}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
