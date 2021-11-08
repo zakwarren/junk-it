977cb33926d1cecb5ccd2dc21fa7e5768bee87f3
zakwarren <41547855+zakwarren@users.noreply.github.com>
2021-11-08 13:41:25 +0000

    Junk service initial setup

---
+++ junk/src/index.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+import { app } from "./app";
+
+const start = async () => {
+  if (!process.env.JWT_PRIVATE_KEY) {
+    throw new Error("JWT_PRIVATE_KEY must be defined");
+  }
+  if (!process.env.JWT_PUBLIC_KEY) {
+    throw new Error("JWT_PUBLIC_KEY must be defined");
+  }
+
+  try {
+    await mongoose.connect("mongodb://junk-mongo-service:27017/junk");
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error(err);
+  }
+
+  const port = process.env.PORT || 3001;
+  app.listen(port, () => console.log(`Listening on port ${port}`));
+};
+
+start();
+
+process.on("SIGINT", async () => {
+  let error = null;
+  try {
+    await mongoose.disconnect();
+    console.log("Disconnecting from MongoDB");
+  } catch (err) {
+    error = err;
+  }
+  console.log("Exiting");
+  process.exit(error ? 1 : 0);
+});
