46ba4a79b5da238b96861906a9ab214945c64111
zakwarren <41547855+zakwarren@users.noreply.github.com>
2021-11-24 15:23:15 +0000

    convert callback to promise

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -6,13 +6,17 @@ console.clear();
 
 const stan = nats.connect("junk-it", "abc", { url: "http://localhost:4222" });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publisher connected to NATS");
 
   const publisher = new JunkCreatedPublisher(stan);
-  publisher.publish({
-    id: "1",
-    title: "Taco made of nachos",
-    price: 100,
-  });
+  try {
+    await publisher.publish({
+      id: "1",
+      title: "Taco made of nachos",
+      price: 100,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
