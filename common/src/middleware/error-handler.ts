a83e954a34f4c7bd916beee5080681e6f8b07195
zakwarren <41547855+zakwarren@users.noreply.github.com>
2021-11-25 14:35:06 +0000

    Log errors

--- common/src/middleware/error-handler.ts
+++ common/src/middleware/error-handler.ts
@@ -8,10 +8,11 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
+  console.error(err);
+
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
-  console.error(err);
   res.status(500).send({ errors: [{ message: "Something went wrong" }] });
 };
