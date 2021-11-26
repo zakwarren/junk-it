680bc60297a3aa790db84dc207e887a478137390
zakwarren <41547855+zakwarren@users.noreply.github.com>
2021-11-26 11:16:06 +0000

    is reserved

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,36 @@
 import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  BadRequestError,
+} from "common";
+
+import { Junk } from "../models";
 
 const router = express.Router();
 
-router.post("/", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/",
+  requireAuth,
+  [body("junkId").not().isEmpty().withMessage("Junk ID must be provided")],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { junkId } = req.body;
+
+    const junk = await Junk.findById(junkId);
+    if (!junk) {
+      throw new NotFoundError();
+    }
+
+    const isReserved = await junk.isReserved();
+    if (isReserved) {
+      throw new BadRequestError("Junk is already reserved");
+    }
+
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,16 @@
 import express, { Request, Response } from "express";
+import { param } from "express-validator";
+import { validateRequest } from "common";
 
 const router = express.Router();
 
-router.get("/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/:orderId",
+  [param("id").isMongoId().withMessage("Please provide a valid order id")],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as showOrderRouter };
