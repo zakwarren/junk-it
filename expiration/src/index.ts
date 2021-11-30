a0b429ab5ada2c3c350ce76a0127e770ca850ed2
zakwarren <41547855+zakwarren@users.noreply.github.com>
2021-11-30 13:24:01 +0000

    order created listener

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -10,6 +10,9 @@ const start = async () => {
   if (!process.env.NATS_URL) {
     throw new Error("NATS_URL must be defined");
   }
+  if (!process.env.REDIS_HOST) {
+    throw new Error("REDIS_HOST must be defined");
+  }
 
   try {
     await natsWrapper.connect(
